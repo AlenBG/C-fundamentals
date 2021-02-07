8bcc39d70cfc21f92b0d2f614609e4db776b0460
AlenBG <57811480+AlenBG@users.noreply.github.com>
2021-02-07 13:56:39 +0200

    Create Zig-Zag-Array.cs

---
+++ Zig-Zag-Array.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+namespace _2._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                
+                if (i % 2 == 0)
+                {
+                    arr1[i] = arr[0];
+                    arr2[i] = arr[1];
+                }
+                else
+                {
+                    arr1[i] = arr[1];
+                    arr2[i] = arr[0];
+                }
+            }
+            Console.WriteLine(String.Join(" ", arr1));
+            Console.WriteLine(String.Join(" ", arr2));
+
+        }
+    }
+}
