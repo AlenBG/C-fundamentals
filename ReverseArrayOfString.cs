b2af1304009f2a6d6c3e9da06be67278df1ecab8
AlenBG <57811480+AlenBG@users.noreply.github.com>
2021-02-07 13:53:44 +0200

    Create ReverseArrayOfString.cs

---
+++ ReverseArrayOfString.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+namespace _4._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Console.Write(text[i] + " ");
+            }
+        }
+    }
+}
